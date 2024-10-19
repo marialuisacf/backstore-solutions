7a02f466b5621facb0a556530801ed98b2cf86ae
maria <mdelacruzf@uoc.edu>
2024-10-19 11:21:24 +0200

    añadimos las excepciones al controladorSocio

--- AA2/src/backsolutions/controlador/ControladorSocio.java
+++ AA2/src/backsolutions/controlador/ControladorSocio.java
@@ -1,4 +1,35 @@
 package backsolutions.controlador;
 
+import backsolutions.modelo.Socio;
+import java.util.List;
+
 public class ControladorSocio {
+    private List<Socio> socios;
+
+    public ControladorSocio(List<Socio> socios) {
+        this.socios = socios;
+    }
+
+    public void addSocio(Socio socio) throws ControladorExcepcion {
+        if (socios.stream().anyMatch(s -> s.getNumSocio() == socio.getNumSocio())) {
+            throw new ControladorExcepcion("El socio con número " + socio.getNumSocio() + " ya existe.");
+        }
+        socios.add(socio);
+        System.out.println("Socio añadido con éxito.");
+    }
+
+    public void deleteSocio(int numSocio) throws ControladorExcepcion {
+        Socio socio = socios.stream()
+                .filter(s -> s.getNumSocio() == numSocio)
+                .findFirst()
+                .orElse(null);
+
+        if (socio == null) {
+            throw new ControladorExcepcion("No se encontró un socio con el número proporcionado.");
+        }
+
+        socios.remove(socio);
+        System.out.println("Socio eliminado con éxito.");
+    }
+
 }
